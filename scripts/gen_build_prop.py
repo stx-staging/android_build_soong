e2185661f15c40fd9588196eb4c82fdbe58ed261
Michael Bestas <mkbestas@lineageos.org>
2024-10-27 17:06:32 +0000

    gen_build_prop: Set ro.statix.device
    Change-Id: I485d74642b869d7ac94ef2fd171cafd759965d41

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -136,6 +136,7 @@ def parse_args():
     config["BuildDisplayId"] = f"{config['BuildId']} {config['BuildKeys']}"
 
   config["BuildDescOverride"] = config["BuildDesc"]
+  config["StatixDevice"] = config["DeviceName"]
 
   override_config(config)
 
@@ -255,6 +256,8 @@ def generate_build_info(args):
   # Only add _asan for a sanitized build if it isn't already a part of the
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
+
+  print(f"ro.statix.device={config['StatixDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
